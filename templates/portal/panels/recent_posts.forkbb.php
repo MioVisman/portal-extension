3b92b5b863bb229b82b2b78ae8d2a56628bdaeff
Visman <visman2008@googlemail.com>
2025-11-02 21:05:46 +0700

    0.5.1

--- templates/portal/panels/recent_posts.forkbb.php
+++ templates/portal/panels/recent_posts.forkbb.php
@@ -4,7 +4,7 @@
             <ul class="f-panel-content">
 @foreach ($topics as $topic)
     @if (! empty($topic))
-              <li class="f-panel-recent-posts-li">
+              <li class="f-panel-recent-posts-li @if (false !== $topic->hasNew) f-fnew @endif">
                 <a class="f-panel-recent-posts-a" href="{{ (false !== $topic->hasNew ? $topic->linkNew : $topic->linkLast) }}" title="{{ __(false !== $topic->hasNew ? 'New posts info' : 'Last post info') }}">{{ $topic->name }}</a>
               </li>
     @endif
